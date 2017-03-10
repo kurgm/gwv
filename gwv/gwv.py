23d92f1a24e919bd1b232cb529dbe022f6cdd463
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-03-10 17:04:13 +0900

    Use dict for dump data format

--- gwv/gwv.py
+++ gwv/gwv.py
@@ -8,6 +8,29 @@ from gwv import version
 from validator import validate
 
 
+def open_dump(filename):
+    dump = {}
+    with open(filename) as f:
+        if filename[-4:] == ".csv":
+            for l in f:
+                row = l.rstrip("\n").split(",")
+                if len(row) != 3:
+                    continue
+                dump[row[0]] = (row[1], row[2])
+        else:
+            # dump_newest_only.txt
+            line = f.readline()  # header
+            line = f.readline()  # ------
+            while line:
+                l = [x.strip() for x in line.split("|")]
+                if len(l) != 3:
+                    line = f.readline()
+                    continue
+                dump[row[0]] = (row[1], row[2])
+                line = f.readline()
+    return dump
+
+
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
@@ -17,17 +40,19 @@ def main(args=None):
     parser.add_argument("dumpfile")
     parser.add_argument("-o", "--out", help="File to write the output JSON to")
     parser.add_argument("-n", "--names", nargs="*", help="Names of validators")
-    parser.add_argument("-v", "--version", action="store_true", help="Names of validators")
+    parser.add_argument("-v", "--version", action="store_true",
+                        help="Names of validators")
     opts = parser.parse_args(args)
 
     if opts.version:
         print(version)
         return
 
-    outpath = opts.out or os.path.join(os.path.dirname(opts.dumpfile), "gwv_result.json")
-    dumpfile = open(opts.dumpfile)
+    outpath = opts.out or os.path.join(
+        os.path.dirname(opts.dumpfile), "gwv_result.json")
+    dump = open_dump(opts.dumpfile)
 
-    result = validate(dumpfile, opts.names or None)
+    result = validate(dump, opts.names or None)
 
     with open(outpath, "w") as outfile:
         outfile.write(result)
