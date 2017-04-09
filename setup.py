6bcc1a72e903e6aa18f24e5ea40817755c33e5cd
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-04-09 16:12:33 +0900

    Add data folder

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from setuptools.command.install import install
 from setuptools import find_packages
 from setuptools import setup
+
+
+class my_install(install):
+
+    def _pre_install(self):
+        pass
+
+    def run(self):
+        self._pre_install()
+        install.run(self)
+
 
 setup(
     name="gwv",
     version="1.0",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             "gwv = gwv.gwv:main"
         ]
     },
+    cmdclass={'install': my_install},
+    package_data={
+        "gwv": ["data/*"],
+    }
 )
