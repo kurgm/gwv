203c35dfe449b9cb1a2fbfff08805dd8162ba6c0
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-07-04 21:33:09 +0900

    [mustrenew] Hoist error codes

--- gwv/validators/mustrenew.py
+++ gwv/validators/mustrenew.py
@@ -7,6 +7,14 @@ from __future__ import unicode_literals
 
 from gwv.validators import filters as default_filters
 from gwv.validators import Validator
+from gwv.validators import ErrorCodes
+
+
+error_codes = ErrorCodes(
+    MUSTRENEW_NO_OLD="0",  # 最新版が旧部品を引用していない部品の旧版を引用している
+    MUSTRENEW_OLD="@",  # 最新版が旧部品を引用している部品の旧版を引用している
+)
+
 
 filters = {
     "alias": {False},
@@ -38,9 +46,11 @@ class MustrenewValidator(Validator):
     def record(self, glyphname, error):
         quotings, quotings_old = error
         for buhinname in quotings:
-            self.results["0"].setdefault(buhinname, []).append(glyphname)
+            self.results[error_codes.MUSTRENEW_NO_OLD].setdefault(
+                buhinname, []).append(glyphname)
         for buhinname in quotings_old:
-            self.results["@"].setdefault(buhinname, []).append(glyphname)
+            self.results[error_codes.MUSTRENEW_OLD].setdefault(
+                buhinname, []).append(glyphname)
 
     def get_result(self):
         return {
