f26859d67a8ef874da937e6eb7f8a7f8d508dd6c
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-07-04 13:59:01 +0900

    [skew] Hoist error codes

--- gwv/validators/skew.py
+++ gwv/validators/skew.py
@@ -10,6 +10,20 @@ import math
 from gwv.helper import isYoko
 from gwv.validators import filters as default_filters
 from gwv.validators import Validator
+from gwv.validators import ErrorCodes
+
+
+error_codes = ErrorCodes(
+    SKEWED_HORI_LINE="10",  # 歪んだ水平
+    SKEWED_VERT_LINE="11",  # 歪んだ垂直
+    SKEWED_HORI_ORE_LAST="30",  # 折れの後半が歪んだ水平
+    SKEWED_VERT_ORE_FIRST="31",  # 折れの前半が歪んだ垂直
+    SKEWED_HORI_OTSU_LAST="40",  # 乙の後半が歪んだ水平
+    HORI_TATEBARAI_FIRST="70",  # 縦払いの直線部分が横
+    SNAPPED_TATEBARAI="71",  # 曲がった縦払い
+    SKEWED_VERT_TATEBARAI_FIRST="72",  # 縦払いの直線部分が歪んだ垂直
+)
+
 
 filters = {
     "alias": {False},
@@ -30,30 +44,31 @@ class SkewValidator(Validator):
                 yDif = abs(data[4] - data[6])
                 if xDif <= yDif and xDif != 0 and xDif <= 3:
                     # 歪んだ垂直
-                    return [11, [line.line_number, line.strdata], round(math.atan2(xDif, yDif) * 180 / math.pi, 1)]
+                    return [error_codes.SKEWED_VERT_LINE, [line.line_number, line.strdata], round(math.atan2(xDif, yDif) * 180 / math.pi, 1)]
                 if xDif > yDif and yDif != 0 and yDif <= 3:
                     # 歪んだ水平
-                    return [10, [line.line_number, line.strdata], round(math.atan2(yDif, xDif) * 180 / math.pi, 1)]
+                    return [error_codes.SKEWED_HORI_LINE, [line.line_number, line.strdata], round(math.atan2(yDif, xDif) * 180 / math.pi, 1)]
             elif stype == 3:
                 xDif1 = abs(data[3] - data[5])
                 yDif1 = abs(data[4] - data[6])
                 if xDif1 != 0 and xDif1 <= 3:
                     # 折れの前半が歪んだ垂直
-                    return [31, [line.line_number, line.strdata], round(math.atan2(xDif1, yDif1) * 180 / math.pi, 1)]
+                    return [error_codes.SKEWED_VERT_ORE_FIRST, [line.line_number, line.strdata], round(math.atan2(xDif1, yDif1) * 180 / math.pi, 1)]
                 xDif2 = abs(data[5] - data[7])
                 yDif2 = abs(data[6] - data[8])
                 if yDif2 != 0 and yDif2 <= 3:
                     # 折れの後半が歪んだ水平
-                    return [30, [line.line_number, line.strdata], round(math.atan2(yDif2, xDif2) * 180 / math.pi, 1)]
+                    return [error_codes.SKEWED_HORI_ORE_LAST, [line.line_number, line.strdata], round(math.atan2(yDif2, xDif2) * 180 / math.pi, 1)]
             elif stype == 4:
                 xDif = abs(data[5] - data[7])
                 yDif = abs(data[6] - data[8])
                 if yDif != 0 and yDif <= 3:
                     # 乙の後半が歪んだ水平
-                    return [40, [line.line_number, line.strdata], round(math.atan2(yDif, xDif) * 180 / math.pi, 1)]
+                    return [error_codes.SKEWED_HORI_OTSU_LAST, [line.line_number, line.strdata], round(math.atan2(yDif, xDif) * 180 / math.pi, 1)]
             elif stype == 7:
                 if isYoko(*data[3:7]):
-                    return [70, [line.line_number, line.strdata]]  # 縦払いの直線部分が横
+                    # 縦払いの直線部分が横
+                    return [error_codes.HORI_TATEBARAI_FIRST, [line.line_number, line.strdata]]
                 xDif1 = data[5] - data[3]
                 yDif1 = data[6] - data[4]
                 theta1 = math.atan2(yDif1, xDif1)
@@ -64,15 +79,15 @@ class SkewValidator(Validator):
                 theta2 = math.atan2(yDif2, xDif2)
                 if (xDif1 == 0 and xDif2 != 0) or abs(theta1 - theta2) * 60 > 3:
                     # 曲がった縦払い
-                    return [71, [line.line_number, line.strdata], round(abs(theta1 - theta2) * 180 / math.pi, 1)]
+                    return [error_codes.SNAPPED_TATEBARAI, [line.line_number, line.strdata], round(abs(theta1 - theta2) * 180 / math.pi, 1)]
                 if xDif1 != 0 and -3 <= xDif1 <= 3:
                     # 縦払いの直線部分が歪んだ垂直
-                    return [72, [line.line_number, line.strdata], round(abs(90 - theta1 * 180 / math.pi), 1)]
+                    return [error_codes.SKEWED_VERT_TATEBARAI_FIRST, [line.line_number, line.strdata], round(abs(90 - theta1 * 180 / math.pi), 1)]
         return False
 
     def get_result(self):
         for key, val in self.results.items():
-            if key != "70":  # not 「縦払いの直線部分が横」
+            if key != error_codes.HORI_TATEBARAI_FIRST:
                 # 歪み角度が大きい順にソート
                 val.sort(key=lambda r: r[2], reverse=True)
         return super(SkewValidator, self).get_result()
