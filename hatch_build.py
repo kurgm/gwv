31a8341831d4a141a68990250f454022a03b8c5b
Kurogoma <gogottt2009-hp@yahoo.co.jp>
2025-04-20 02:26:13 +0900

    Run build hook on all build targets

--- hatch_build.py
+++ hatch_build.py
@@ -8,13 +8,8 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
-        if self.target_name == "sdist":
-            sdist_build()
+        repo_root = pathlib.Path(__file__).parent
+        sys.path.append(str(repo_root))
+        import bdat
 
-
-def sdist_build():
-    repo_root = pathlib.Path(__file__).parent
-    sys.path.append(str(repo_root))
-    import bdat
-
-    bdat.main()
+        bdat.main()
